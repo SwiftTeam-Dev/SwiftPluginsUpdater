8ec41a7e0004e83cd3211da179dfe4b735f31e09
Sniper_TVmc <snipertv59.pro@gmail.com>
2023-12-28 00:58:08 +0100

    Create ConsoleLogger.java

---
+++ src/main/java/fr/snipertvmc/swiftpluginsupdater/utilities/ConsoleLogger.java
@@ -0,0 +1,22 @@
+package fr.snipertvmc.swiftpluginsupdater.utilities;
+
+import org.bukkit.Bukkit;
+
+public class ConsoleLogger {
+
+	public void console(String infoMessage) {
+		Bukkit.getConsoleSender().sendMessage("§8§l[§6SwiftPluginsUpdater§8§l]§7 " + infoMessage);
+	}
+
+	public void info(String infoMessage) {
+		Bukkit.getLogger().info("[SwiftPluginsUpdater] " + infoMessage);
+	}
+
+	public void warning(String infoMessage) {
+		Bukkit.getLogger().warning("[SwiftPluginsUpdater] " + infoMessage);
+	}
+
+	public void error(String infoMessage) {
+		Bukkit.getLogger().severe("[SwiftPluginsUpdater] " + infoMessage);
+	}
+}
